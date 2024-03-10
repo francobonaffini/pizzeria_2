68bd541c8e6ca8f1c53dc271eed4465814a706d3
Franco Bonaffini <codigofranco1@gmail.com>
2024-03-10 16:43:30 -0300

    second commit

--- pizza_project/wsgi.py
+++ pizza_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizza_project.pizza_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizza_project.settings')
 
 application = get_wsgi_application()
